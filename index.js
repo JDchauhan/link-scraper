c1c592718cad16e8ffe44c078c77343b0f7b9d8a
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-07-25 10:49:42 +0530

    fixes #1

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 function getAllLinks(url){
-    var url = "https://www.npmjs.com/package/easy-screen-capture";
+    urls = [];
+    var obj = {};
     var baseUrl = url.split("/");
     baseUrl = baseUrl[0] + "//" + baseUrl[2];
     request(url, function(err, resp, body){
         $ = cheerio.load(body);
         links = $('a'); //jquery get all hyperlinks
         $(links).each(function(i, link){
+            var name = $(link).text();
             var link = $(link).attr('href');
             if(link.indexOf("/") === 0){
                 link = baseUrl + link;
@@ -17,6 +19,10 @@ function getAllLinks(url){
             if(link.indexOf("?") === 0 || link.indexOf("#") === 0){
                 link = url + link;
             }
+            obj[name] = link;
+            urls.push(obj);
+            obj = {};
         });
+        return urls;
     });
 }
