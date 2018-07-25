323db5fc753e33cd1960517bd20930c39f7a2f64
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-07-25 19:11:15 +0530

    fixes #5

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-function getAllLinks(url){
-    urls = [];
+function getAllLinks(url,callback) {
+    var urls = [];
     var obj = {};
     var baseUrl = url.split("/");
     baseUrl = baseUrl[0] + "//" + baseUrl[2];
-    request(url, function(err, resp, body){
+    request(url, function (err, resp, body) {
         $ = cheerio.load(body);
         links = $('a'); //jquery get all hyperlinks
-        $(links).each(function(i, link){
+        $(links).each(function (i, link) {
             var name = $(link).text();
             var link = $(link).attr('href');
-            if(link.indexOf("/") === 0){
+            if (link.indexOf("/") === 0) {
                 link = baseUrl + link;
             }
 
-            if(link.indexOf("?") === 0 || link.indexOf("#") === 0){
+            if (link.indexOf("?") === 0 || link.indexOf("#") === 0) {
                 link = url + link;
             }
             obj[name] = link;
             urls.push(obj);
             obj = {};
+            if(links.length === urls.length){
+                callback(urls);
+            }
         });
-        return urls;
     });
 }
